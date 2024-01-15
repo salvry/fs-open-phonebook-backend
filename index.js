68ca5d301bf1ab2385209111454cfd73c108f13d
unknown <sallaryytty@gmail.com>
2024-01-15 14:14:18 +0200

    small updates

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('dist'))
 
 morgan.token('content', function getContent(req) {
     return JSON.stringify(req.body)
